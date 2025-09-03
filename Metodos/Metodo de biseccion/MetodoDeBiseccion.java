17c63de3d1a5b259f486535b3a18a235e2bc3251
Milton-Peluffo <milton.peluffoc@gmail.com>
2025-09-02 21:54:05 -0500

    feat: Añadiendo el codigo del metodo de biseccion

--- Metodos/Metodo de biseccion/MetodoDeBiseccion.java
+++ Metodos/Metodo de biseccion/MetodoDeBiseccion.java
@@ -4,7 +4,7 @@ import java.util.function.Function;
 public class MetodoDeBiseccion {
 
     public static void main(String[] args) {
-        System.out.println("=== MÉTODO DE BISECCIÓN ===");
+        System.out.println("==== MÉTODO DE BISECCIÓN ====");
 
         Function<Double, Double> funcion = (x) -> Math.pow(x, 3) + x - 1;
 
